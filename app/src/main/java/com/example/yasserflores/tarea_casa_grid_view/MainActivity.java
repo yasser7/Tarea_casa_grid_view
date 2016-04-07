e43a50c86ed76759e33673b6759feab206928bc7
Miller Flores <Miller Flores>
2016-04-06 19:42:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/yasserflores/tarea_casa_grid_view/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.yasserflores.tarea_casa_grid_view;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.GridView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
